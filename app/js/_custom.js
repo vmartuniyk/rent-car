6efc8c7610ad5a4cfc51653050c329edaff922e3
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-02-04 14:01:30 +0100

    Зроблено адаптив для мобільних та планшетів

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
-
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+    navToggle.on("click",function(event){
+       event.preventDefault();
+       nav.toggleClass("show");
+   });
 });
