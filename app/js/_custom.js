f6c8271e441e05fe96396af18475d31c7a307752
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-02-05 14:22:27 +0100

    Додав слайдер для секції автомобілів

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -2,19 +2,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
     let nav = $("#nav");
+    let header__phone = $("#header__phone");
     let navToggle = $("#navToggle");
     navToggle.on("click",function(event){
        event.preventDefault();
        nav.toggleClass("show");
+       header__phone.toggleClass("show");
    });
     
     //Portfolio slider
     
-    let portfolioSlider = $("#service__slider");
+    let serviceSlider = $("#service__slider");
+    
+    serviceSlider.slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false
+    });
+    
+     let portfolioSlider = $("#porfolio_slider");
     
     portfolioSlider.slick({
       infinite: true,
-      slidesToShow: 1,
+      slidesToShow: 3,
       slidesToScroll: 1,
       dots: true,
       arrows: false
